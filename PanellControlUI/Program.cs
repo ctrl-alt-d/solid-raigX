3aacc8d5c39f4a55a5221fce3ccbc8c53d1c804a
Dani H <ctrl.alt.d@gmail.com>
2026-06-17 19:28:50 +0200

    Afegir injecció de dependències
    Co-authored-by: Santi Rivas (2) <santirvs@users.noreply.github.com>
    Co-authored-by: ajuvi <ajuvi@users.noreply.github.com>

--- PanellControlUI/Program.cs
+++ PanellControlUI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ControladoraRaigX;
 using MaquinaRaigX;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace PanellControlUI;
 
@@ -8,9 +9,19 @@ class Program
     static async Task Main(string[] args)
     {
         _ = args;
-        var maquina = new Maquina();
-        var controladora = new Controladora(maquina);
-        var app = new App(controladora);
+        
+        var services = new ServiceCollection();
+
+        services.AddSingleton<IMaquina, Maquina>();
+        services.AddSingleton<App>();
+        services.AddSingleton<Controladora>();
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        var app = serviceProvider.GetRequiredService<App>();
+
         await app.Main();
+        
     }
+    
 }
