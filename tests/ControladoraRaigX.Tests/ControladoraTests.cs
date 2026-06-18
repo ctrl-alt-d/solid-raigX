7a2cea6869d6190031d1e2edd0bc3a33d4f822c7
Dani H <ctrl.alt.d@gmail.com>
2026-06-18 17:20:35 +0200

    feat: test SiTotEsCorrecteSAplicaLaRadiacio

--- tests/ControladoraRaigX.Tests/ControladoraTests.cs
+++ tests/ControladoraRaigX.Tests/ControladoraTests.cs
@@ -1,18 +1,42 @@
-﻿namespace ControladoraRaigX.Tests;
+﻿using MaquinaRaigX;
+using NSubstitute;
+
+namespace ControladoraRaigX.Tests;
+
 
 public class ControladoraTests
 {
-
-
     [Fact]
     public async Task SiTotEsCorrecteSAplicaLaRadiacio()
     {
+        // arrange
+        var maquina = Substitute.For<IMaquina>();
+        maquina.ComprovaTotsElsSistemesActius().Returns(true);
+        var controladora = new Controladora(maquina);
+
+        // act
+        await controladora.AplicaRadiació(80, 180, CancellationToken.None);
+
+        // assert
+        maquina.Received(1).ComprovaTotsElsSistemesActius();
+        await maquina.Received(1).AplicaRadiació(Arg.Any<int>(), Arg.Any<int>(), Arg.Any<CancellationToken>());
+
+        Received.InOrder(() => {
+            maquina.ComprovaTotsElsSistemesActius();
+            maquina.AplicaRadiació(Arg.Any<int>(), Arg.Any<int>(), Arg.Any<CancellationToken>());
+        });
     }
 
 
     [Fact]
     public async Task SiLaMaquinaNoEstaOkLaControladoraLLançaUnaExcepcioInoAplicaRadiació()
     {
+        // arrange
+
+
+        // act
+
+        //
 
     }
 
