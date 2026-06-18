e1bfbd45d4877e4cf77309c9a3a7ff6eb993614b
Dani H <ctrl.alt.d@gmail.com>
2026-06-18 17:29:57 +0200

    Feat: documentar mètode

--- tests/ControladoraRaigX.Tests/ControladoraTests.cs
+++ tests/ControladoraRaigX.Tests/ControladoraTests.cs
@@ -18,9 +18,11 @@ public class ControladoraTests
         await controladora.AplicaRadiació(80, 180, CancellationToken.None);
 
         // assert
-        maquina.Received(1).ComprovaTotsElsSistemesActius();
+        // La controladora ha d'haver demanat a la màquina que emeti radiació
         await maquina.Received(1).AplicaRadiació(Arg.Any<int>(), Arg.Any<int>(), Arg.Any<CancellationToken>());
-
+        // La controladora ha d'haver preguntat a la màquina si té tots els sistemes actius
+        maquina.Received(1).ComprovaTotsElsSistemesActius();
+        // L'ordre en que la controladora crida a la màquina és:
         Received.InOrder(() => {
             maquina.ComprovaTotsElsSistemesActius();
             maquina.AplicaRadiació(Arg.Any<int>(), Arg.Any<int>(), Arg.Any<CancellationToken>());
